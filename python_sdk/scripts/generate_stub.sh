63c66948fe0de635b88ef069bdb0aaeef1d60d98
Richard Jiang <rjiang@salesforce.com>
2025-04-28 16:45:48 -0700

    update path in generate_stub.sh

--- python_sdk/scripts/generate_stub.sh
+++ python_sdk/scripts/generate_stub.sh
@@ -10,7 +10,7 @@ set -x
 openapi-generator-cli generate \
     -i ../VizQLDataServiceOpenAPISchema.json \
     -g python-pydantic-v1 \
-    --ignore-file-override .openapi-generator-ignore \
+    --ignore-file-override ../.openapi-generator-ignore \
     --additional-properties=generateSourceCodeOnly=true,packageName=openapi_client,projectName=openapi_client
 
 echo "OpenAPI client generation completed successfully"
