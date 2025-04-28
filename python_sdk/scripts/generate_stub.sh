ffb72e178b7ec710e259f89f655b61a7a2882957
Yunlong Zhang <yunlong.zhang@salesforce.com>
2025-04-28 11:52:40 -0700

    temp fix 2

--- python_sdk/scripts/generate_stub.sh
+++ python_sdk/scripts/generate_stub.sh
@@ -11,7 +11,7 @@ which pip
 pip list  # Check installed packages
 
 # Generate client code
-python -m openapi-generator-cli generate \
+openapi-generator-cli generate \
     -i VizQLDataServiceOpenAPISchema.json \
     -g python-pydantic-v1 \
     -o python_sdk/build/generated \
