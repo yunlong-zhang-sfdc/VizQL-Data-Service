fa96be42c2e4e43bd8d470a9050e0f52c34d97ee
Yunlong Zhang <yunlong.zhang@salesforce.com>
2025-05-08 12:24:27 -0700

    style fix

--- python_sdk/src/client.py
+++ python_sdk/src/client.py
@@ -12,7 +12,8 @@ from .server import Server
 
 API_SUBDOMAIN = "/api/v1/vizql-data-service"
 X_TABLEAU_AUTH = "X-Tableau-Auth"
-VERSION="1.0.0" # hardcoded for now
+VERSION = "1.0.0"  # hardcoded for now
+
 
 class Client:
     """Client for VizQL Data Service API."""
